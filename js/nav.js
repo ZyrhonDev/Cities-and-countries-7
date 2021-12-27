9af24e2047a6a71246ce14823d5d83b8631e34f9
ZyrhonDev <93902196+ZyrhonDev@users.noreply.github.com>
2021-12-27 14:12:49 +0100

    skapade struktur för meny

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,10 @@
 /* 
 Skapa <nav> och appenda till #siteHeader
 */ 
+const siteHead = document.getElementById ("siteHeader");
+const countryMenu = document.createElement ("nav");
+
+countryMenu.innerText = "Hej";
+
+siteHead.append (countryMenu);
+
