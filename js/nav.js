6da6b59643ea06843a2357642ac5a6c40138255e
Mikaela-e <tengoos@hotmail.com>
2021-12-27 20:01:07 +0100

    Ej klart

--- js/nav.js
+++ js/nav.js
@@ -3,8 +3,16 @@ Skapa <nav> och appenda till #siteHeader
 */ 
 const siteHead = document.getElementById ("siteHeader");
 const countryMenu = document.createElement ("nav");
-
 countryMenu.innerText = "Hej";
+
+///IN PROGRESS
+// let visitedCities = document.createElement("button")
+// visitedCities.innerText ="Besökta Städer";
+
+// visitedCities.addEventListener("click", ()=>{
+
+// })
+// countryMenu.append(visitedCities);
 
 siteHead.append (countryMenu);
 
