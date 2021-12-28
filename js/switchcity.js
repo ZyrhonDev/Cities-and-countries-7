85a4b8c933a60ef097bd43589a7caecc9038c9cd
Eraze86 <therese.rosvall@hotmail.se>
2021-12-28 08:27:36 +0100

    swish citys info with a click

---
+++ js/switchcity.js
@@ -0,0 +1,55 @@
+//clicka på en stad med evt.target.id
+//eventlistner till ul?
+
+addEventListener("click", (evt) => {
+    console.log(evt)
+    switch (evt) {
+        //när man trycker på id, visas rätt stad info 
+        case "1":
+        //visa population
+        //ev bild, och väderlek 
+            break;
+        case "2":
+
+            break;
+        case "3":
+
+            break;
+        case "4":
+
+            break;
+        case "5":
+
+            break;
+        case "6":
+
+            break;
+        case "7":
+
+            break;
+        case "8":
+
+            break;
+        case "9":
+
+            break;
+        case "10":
+
+            break;
+        case "11":
+
+            break;
+        case "12":
+
+            break;
+        case "13":
+
+            break;
+        case "14":
+
+            break;
+        case "15":
+
+            break;
+    }
+})
