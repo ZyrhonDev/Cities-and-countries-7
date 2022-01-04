353002e2570eaf45a45f77b5f70484f938bfc9cd
Eraze86 <therese.rosvall@hotmail.se>
2022-01-04 16:01:48 +0100

    fixed css

--- js/wikiApi.js
+++ js/wikiApi.js
@@ -6,21 +6,19 @@ export async function wikiApi(infoObject) {
         let cityInfoContainer = document.getElementById("infoContainer"); 
             // cityInfoContainer.innerHTML=""; 
         let cityText = document.createElement("article")
-            cityText.style.cssText = "position: absolute; top: 3rem; right: 15rem; width: 50rem;"; 
             cityText.innerText = infoObject.extract;
             cityText.className = "city-info";
-        cityInfoContainer.append(cityText);
+        
 
         //Hämtar original bildens bredd och höjd, för att behålla aspect ratio                
         // let width = infoObject.originalimage.width;
         // let height = infoObject.originalimage.height; 
-        let conatinerOfPic = document.getElementById("picContainer");
-            conatinerOfPic.innerHTML=""; 
+        
         let cityPic = new Image();
             cityPic.src = infoObject.originalimage.source;
-            conatinerOfPic.style.cssText = "position: absolute; top: 26rem; left: 40rem; width: 50rem;"; 
+            
             cityPic.className = "city-pic";
-        conatinerOfPic.append(cityPic)
+            cityInfoContainer.append(cityText, cityPic);
             
     // console.log(infoObject.extract)
     // console.log(infoObject.originalimage)
