a13b7c1d58ff86f972a27605a340f661e2e5f58c
Kirill M <94078612+FuturisticZigZag@users.noreply.github.com>
2022-01-04 15:11:56 +0100

    Remade and moved part of the code to clickOnCity

--- js/wikiApi.js
+++ js/wikiApi.js
@@ -1,49 +1,28 @@
-export async function wikiApi() {
+export async function wikiApi(infoObject) {      
+    
+    //.extract och .originalimage.source är texten resp bilden från "wiki api json"
+    // function cityInfo(infoObject) {
+        
+        let cityInfoContainer = document.getElementById("infoContainer"); 
+            // cityInfoContainer.innerHTML=""; 
+        let cityText = document.createElement("article")
+            cityText.style.cssText = "position: absolute; top: 3rem; right: 15rem; width: 50rem;"; 
+            cityText.innerText = infoObject.extract;
+            cityText.className = "city-info";
+        cityInfoContainer.append(cityText);
 
-    //Selektar alla ellement med classen cityClass (så alla städer)
-    let listItems = document.querySelectorAll(".city");
-    console.log(listItems)
-    //Skapar en click event listener till varje selected element
-    listItems.forEach(element => {
-        element.addEventListener("click", (e) => {        
-            // console.log(e.target.textContent);
-
-            //Hämtar text innehållet från det man clickar på
-            let userChoice = e.target.textContent
-
-            //Fetchar wiki api och ersätter sista delen i adressen med stadsnamnet man har clickat på
-             fetch("https://en.wikipedia.org/api/rest_v1/page/summary/" + userChoice)
-            .then((response) => response.json())
-            .then((data) => cityInfo(data));
-
-
-            //.extract och .originalimage.source är texten resp bilden från "wiki api json"
-            function cityInfo(infoObject) {
-                let cityInfoContainer = document.getElementById("siteMain");   
-                    cityInfoContainer.innerHTML ="";
-                let cityText = document.createElement("article")
-                    cityText.style.cssText = "position: absolute; top: 3rem; right: 15rem; width: 50rem;"; 
-                    cityText.innerText = infoObject.extract;
-                    cityText.className = "city-info";
-
-                //Hämtar original bildens bredd och höjd, för att behålla aspect ratio
-                //Delar på 5 för att få en mindre bild 
-                // let width = infoObject.originalimage.width;
-                // let height = infoObject.originalimage.height; 
-                let conatinerOfPic = document.createElement("div") ;  
-                let cityPic = new Image();
-                    cityPic.src = infoObject.originalimage.source;
-                    conatinerOfPic.style.cssText = "position: absolute; top: 26rem; left: 40rem; width: 50rem;"; 
-                    cityPic.className = "city-pic";
-                    conatinerOfPic.append(cityPic)
-                    cityInfoContainer.append(cityText, conatinerOfPic);
-                
-                
-                
-            // console.log(infoObject.extract)
-            // console.log(infoObject.originalimage)
-            }
-        })
-    })
-
+        //Hämtar original bildens bredd och höjd, för att behålla aspect ratio                
+        // let width = infoObject.originalimage.width;
+        // let height = infoObject.originalimage.height; 
+        let conatinerOfPic = document.getElementById("picContainer");
+            conatinerOfPic.innerHTML=""; 
+        let cityPic = new Image();
+            cityPic.src = infoObject.originalimage.source;
+            conatinerOfPic.style.cssText = "position: absolute; top: 26rem; left: 40rem; width: 50rem;"; 
+            cityPic.className = "city-pic";
+        conatinerOfPic.append(cityPic)
+            
+    // console.log(infoObject.extract)
+    // console.log(infoObject.originalimage)
+    // };
 };
