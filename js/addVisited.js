073343803145c3ba6b11975c0e53a76d9995a190
ZyrhonDev <93902196+ZyrhonDev@users.noreply.github.com>
2022-01-04 18:44:16 +0100

    localStorage WiP, lägga till klart. Visa ännu inte färdig

--- js/addVisited.js
+++ js/addVisited.js
@@ -1,23 +1,44 @@
-// Hämtar "besökt land" knappen med getElementById
-export function addVisitedCity() {
+
+export function addVisitedCity(cities) {
     const addVisited = document.createElement ("button");
-    addVisited.textContent = "Lägg till"
+    addVisited.textContent = "Besökt"
     addVisited.id = "addVisited"
     addVisited.style.cssText = "position: absolute; top: 1rem; right: 1rem;"; 
 
-    const visitedCities = [];
-    const selectedCity = document.getElementById ("countryName");
-
-    body.document.append(addVisited)
-//Eventlistener på "besökt land" knappen
-    addVisited.addEventListener ("click", function() {
+    document.body.append(addVisited)
+    // Hämta localStorage och OM det redan finns något i den så parsar den och sparas i visitedCities, så vi kan lägga till saker efter det som redan finns.
+    
+    
+   
+   function getListFromLS() {
     let getCityList = localStorage.getItem("cityList");
 
-    if(getCityList) {
+    let visitedCities = [];
+
+    if (getCityList) {
         visitedCities = JSON.parse(getCityList);
     }
+    return visitedCities;
+   }
 
-    visitedCities.push(selectedCity);
-    localStorage.setItem("cityList", JSON.stringify(visitedCities));
+   let visitedCities = getListFromLS();
+
+// ########################################################################
+// ## LÄGGER TILL STÄDERS ID I LOCALSTORAGE, DUBLETTER KAN DOCK FÖREKOMMA##
+// ########################################################################
+    addVisited.addEventListener ("click", function() {
+        
+    let selectedCity = localStorage.getItem("selectedCity");
+
+    for (let p = 0; p < cities.length; p++) {
+        if (selectedCity == cities[p].stadname) {
+            visitedCities.push(cities[p].id)
+            console.log(visitedCities);
+            localStorage.setItem("cityList", JSON.stringify(visitedCities));
+            
+        }
+    }
+    // visitedCities.push(selectedCity);
+    // localStorage.setItem("cityList", JSON.stringify(visitedCities));
     })
 }
--- js/clickOnCity.js
+++ js/clickOnCity.js
@@ -1,5 +1,4 @@
 import { wikiApi } from './wikiApi.js';
-
 export async function addCityInfoWrapper(cities) {
     let populationContainer = document.createElement("div");
         populationContainer.id = "populationContainer";
@@ -12,24 +11,24 @@ export async function addCityInfoWrapper(cities) {
 
     document.getElementById("siteMain").append(
     populationContainer, weatherContainer, infoContainer, picContainer);    
-
+    
     //Selektar alla ellement med classen city (så alla städer)
     let listItems = document.querySelectorAll(".city");
     // console.log(listItems)
-    
+
     //Skapar en click event listener till varje selected element
     listItems.forEach(element => {
         element.addEventListener("click", (e) => {        
             // console.log(e.target.textContent);            
             
             //Hämtar text innehållet från det man clickar på
-            let userChoice = e.target.textContent
-            
+            let userChoice = e.target.textContent;
+            let selectedCity = localStorage.setItem("selectedCity", e.target.textContent);
             //Printar population
             for (var i in cities){
                 let populationContainer = document.getElementById("populationContainer"); 
                 let cityHeading = document.createElement("h2");
-                    cityHeading.innerText = cities[i].stadname;    
+                    cityHeading.innerText = cities[i].stadname;
                 let populationNumber = document.createElement("p");             
                     populationNumber.innerText = `Population: ${cities[i].population}`;
                 let x = cities[i].stadname;
